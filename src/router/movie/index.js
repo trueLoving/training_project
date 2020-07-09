89c77206096fe88fc8c3ad8ce6ade9b0e276585a
star sky <1176789241@qq.com>
2020-07-09 19:56:35 +0800

    删除冗余文件

--- src/router/movie/index.js
+++ src/router/movie/index.js
@@ -1,27 +1,18 @@
 export default {
-    path : '/movie',
-    component : () => import('@/views/Movie'),
-    children : [
+    path: '/movie',
+    component: () => import('@/views/Movie'),
+    children: [
         {
-            path : 'city',
-            component : () => import('@/components/City')
+            path: 'nowPlaying',
+            component: () => import('@/components/NowPlaying')
         },
         {
-            path : 'nowPlaying',
-            component : () => import('@/components/NowPlaying')
+            path: 'comingSoon',
+            component: () => import('@/components/ComingSoon')
         },
         {
-            path : 'comingSoon',
-            component : () => import('@/components/ComingSoon')
-        },
-        {
-            path : 'search',
-            component : () => import('@/components/Search')
-        },
-
-        {
-            path : '/movie',
-            redirect : '/movie/nowPlaying'
+            path: '/movie',
+            redirect: '/movie/nowPlaying'
         }
     ]
 }
